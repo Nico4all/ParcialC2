57d51e9651c370c04277cbf68b243a7aad10591a
nico <esteban.sanchez@gers.com>
2024-10-19 11:35:27 -0500

    migraciones

---
+++ database/migrations/2024_10_19_162945_create__cursos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  
+    public function up(): void
+    {
+        Schema::create('_cursos', function (Blueprint $table) {
+            $table->id();
+            $table->string('título');
+            $table->text('descripción')->nullable();
+            $table->integer('duración'); // asumiendo que la duración es en horas
+            $table->decimal('precio', 8, 2);
+            $table->string('categoría');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('_cursos');
+    }
+};
